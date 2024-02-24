c8906e1fc15f88e0bd0b3d5b58f414c4791209bd
seyun4047 <love4047@naver.com>
2024-02-25 04:40:12 +0900

    [Fixed]main templates 수정을 통한 lab 접근방식 구현

--- cloudWeb/cloud/urls.py
+++ cloudWeb/cloud/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('sharepage.urls')),
-    path('login/', include('common.urls'))
+    path('login/', include('common.urls')),
+    path('lab/', include('lab.urls'))
 ]
