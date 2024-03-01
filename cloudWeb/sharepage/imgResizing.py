f9aa095d7b21e0ed8e93243f35d130c6dc29f294
seyun4047 <love4047@naver.com>
2024-03-01 17:52:50 +0900

    [UPDATE] 누락된 thumnail 구현을 위한 imgResizing 모듈 추가 및 제거된 파일들 업데이트

---
+++ cloudWeb/sharepage/imgResizing.py
@@ -0,0 +1,8 @@
+import cv2
+def resizeImg(imgSrc):
+    inputImg = cv2.imread(imgSrc)
+    oriH, oriW = inputImg.shape[:2]
+    resizedImg = cv2.resize(inputImg, (int(oriW * 0.2), int(oriH * 0.2)), interpolation=cv2.INTER_AREA)
+    new_path = imgSrc.split('.')[0]+"_t."+imgSrc.split('.')[1]
+    # print("this new_path:",new_path)
+    cv2.imwrite(new_path, resizedImg)
